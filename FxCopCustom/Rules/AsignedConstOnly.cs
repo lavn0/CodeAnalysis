b877ae85a22430694370c192568bd60143658302
LAVN <rairairakurakulilac@hotmail.co.jp>
2015-11-23 23:50:06 +0900

    固定値のみが代入されたローカル値を検出するルール(AsignedConstOnly)を追加。

---
+++ FxCopCustom/Rules/AsignedConstOnly.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.FxCop.Sdk;
+using System.Collections.Generic;
+
+namespace FxCopCustom.Rules
+{
+	/// <summary>固定値のみが代入されたローカル値を検出する</summary>
+	public class AsignedConstOnly : BaseRule
+	{
+		private Dictionary<Local, Literal> localsOfLiteral = new Dictionary<Local, Literal>();
+
+		public AsignedConstOnly()
+				: base(typeof(AsignedConstOnly).Name)
+		{
+		}
+
+		public override ProblemCollection Check(Member member)
+		{
+			if (member != null)
+			{
+				switch (member.NodeType)
+				{
+					case NodeType.Method:
+						this.Visit(member);
+						foreach (var local in this.localsOfLiteral)
+						{
+							if (local.Value != null &&
+								!Microsoft.FxCop.Sdk.RuleUtilities.IsCompilerGenerated(local.Key))
+							{
+								this.Violate(local.Key, local.Key.Name.Name);
+							}
+						}
+
+						this.localsOfLiteral.Clear();
+						break;
+				}
+			}
+
+			return base.Check(member);
+		}
+
+		public override void VisitAssignmentStatement(AssignmentStatement assignment)
+		{
+			base.VisitAssignmentStatement(assignment);
+
+			if (assignment == null)
+			{
+				return;
+			}
+
+			var targetLocal = assignment.Target as Local;
+			if (targetLocal == null)
+			{
+				return;
+			}
+
+			var sourceLiteral = assignment.Source as Literal;
+			this.localsOfLiteral[targetLocal] = sourceLiteral;
+		}
+	}
+}
---
+++ ViolationLibrary/AsignedConstOnlyTest.cs
@@ -0,0 +1,45 @@
+﻿namespace ViolationLibrary
+{
+	public static class AsignedConstOnlyTest
+	{
+		public static string NG1()
+		{
+			var local = "a";
+			return local;
+		}
+
+		public static string NG2()
+		{
+			var local = "a";
+			local = "b";
+			return local;
+		}
+
+		public static int NG3()
+		{
+			var local = 0;
+			local = 1;
+			return local;
+		}
+
+		public static string OK1()
+		{
+			const string local = "a";
+			return local;
+		}
+
+		public static string OK2()
+		{
+			var local = "a";
+			local += "b";
+			return local;
+		}
+
+		public static int OK3()
+		{
+			var local = 0;
+			local++;
+			return local;
+		}
+	}
+}
