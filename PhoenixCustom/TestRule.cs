9ec14e250c644cec9c066a5a86d613f5a4c97d3c
LAVN <rairairakurakulilac@hotmail.co.jp>
2016-02-16 00:28:34 +0900

    PhoenixRuleのカスタムルールの最低限の実装を追加

---
+++ PhoenixCustom/TestRule.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.CodeAnalysis.Extensibility;
+using Microsoft.VisualStudio.CodeAnalysis.Phoenix.Extensibility;
+using Phx;
+
+namespace PhoenixCustom
+{
+	/// <summary>実験的に実装された最低限の設定のPhoenixRule</summary>
+	[LocalizedFxCopRule("PhenixCustom.XX0001", typeof(ReliabilityCategory))]
+	internal sealed class TestRule : PhoenixRule
+	{
+	private readonly StatisticsService m_statisticsService;
+
+	public TestRule(StatisticsService statisticsService)
+	{
+		this.m_statisticsService = statisticsService;
+	}
+
+	[FunctionUnitTask(FunctionUnitTargetState.Dataflow)]
+	private void AnalyzeFunction(
+		FunctionUnit functionUnit,
+		WarningEmitter warningEmitter)
+	{
+	}
+	}
+}
