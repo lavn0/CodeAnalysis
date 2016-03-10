063ea5346b903ac24948829a0bbb5c1ff4e91af8
LAVN <rairairakurakulilac@hotmail.co.jp>
2016-03-10 23:04:57 +0900

    StyleCopカスタムルール"EX0001" PreferAtmarkQuotedString 実装

---
+++ StyleCopCustom/PreferAtmarkQuotedString.cs
@@ -0,0 +1,37 @@
+﻿using StyleCop;
+using StyleCop.CSharp;
+using System.Text.RegularExpressions;
+
+namespace StyleCopCustom
+{
+	[SourceAnalyzer(typeof(CsParser))]
+	public class PreferAtmarkQuotedString : SourceAnalyzer
+	{
+		const string pattern = @"(?!^.*(?<=(?<!\\)(?:\\\\)*?)\\[^\\].*$)^([""']).*\\\\.*\1$";
+		private static Regex regexPattern;
+		private static Regex RegexPattern { get { return regexPattern = regexPattern ?? new Regex(pattern); } }
+
+		public override void AnalyzeDocument(CodeDocument document)
+		{
+			CsDocument csdocument = (CsDocument)document;
+			if (csdocument.RootElement != null && !csdocument.RootElement.Generated)
+			{
+				csdocument.WalkDocument(null, null, new CodeWalkerExpressionVisitor<object>(this.VistExpression), null);
+			}
+		}
+
+		private bool VistExpression(Expression expression, Expression parentExpression, Statement parentStatement, CsElement parentElement, object context)
+		{
+			var literal = expression as LiteralExpression;
+			if (literal != null)
+			{
+				if (RegexPattern.IsMatch(literal.Text))
+				{
+					this.AddViolation(parentElement, literal.Location, this.GetType().Name);
+				}
+			}
+
+			return true;
+		}
+	}
+}
