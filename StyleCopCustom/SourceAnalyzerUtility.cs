cf035ae7ca2bbf0076d81ff1ab62a469c61bcab7
LAVN <rairairakurakulilac@hotmail.co.jp>
2016-04-03 15:29:29 +0900

    StyleCopCustom デバッグ出力メソッド修正

--- StyleCopCustom/SourceAnalyzerUtility.cs
+++ StyleCopCustom/SourceAnalyzerUtility.cs
@@ -1,13 +1,26 @@
 ﻿using StyleCop;
 using StyleCop.CSharp;
+using System.Diagnostics;
+using System.Linq;
 
 namespace StyleCopCustom
 {
 	public static class SourceAnalyzerUtility
 	{
-		internal static void Violate(this SourceAnalyzer sourceAnalyzer, CsElement elemnet, CodeUnit codeUnit)
+		internal static void Violate(this SourceAnalyzer sourceAnalyzer, CsElement element, CodeUnit codeUnit)
 		{
-			sourceAnalyzer.AddViolation(elemnet, codeUnit.Location, sourceAnalyzer.GetType().Name);
+			sourceAnalyzer.AddViolation(element, codeUnit.Location, sourceAnalyzer.GetType().Name);
+			DebugWrite(element.Violations.Last());
+		}
+
+		[Conditional("DEBUG")]
+		private static void DebugWrite(Violation violation, params object[] args)
+		{
+			Debug.WriteLine(
+				"{0} ({1}): {2}",
+				violation.SourceCode.Path,
+				violation.Line,
+				string.Format(violation.Message, args));
 		}
 	}
 }
