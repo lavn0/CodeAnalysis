9d0fad65b9aae3d07c1744605a5b2d6d1e360321
LAVN <rairairakurakulilac@hotmail.co.jp>
2016-10-18 23:26:24 +0900

    StyleCopCustomプロジェクトでカスタムルール (WideSpaceNotBeUsed) を作成

---
+++ StyleCopCustom/Rules/WideSpaceNotBeUsed.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.ObjectModel;
+using StyleCop;
+using StyleCop.CSharp;
+using StyleCopCustom.Settings;
+
+namespace StyleCopCustom.Rules
+{
+	[SourceAnalyzer(typeof(CsParser), "StyleCopCustom.Rules.xml")]
+	public class WideSpaceNotBeUsed : SourceAnalyzer
+	{
+		private static readonly ReadOnlyCollection<XPathRule> rules;
+
+		static WideSpaceNotBeUsed()
+		{
+			rules = StyleCopsettingsReader.Settings.XPathRules;
+		}
+
+		public override void AnalyzeDocument(CodeDocument document)
+		{
+			CsDocument csdocument = (CsDocument)document;
+			if (csdocument.RootElement != null && !csdocument.RootElement.Generated)
+			{
+				this.CheckSpacing(csdocument.Tokens, false, null);
+			}
+		}
+
+		private void CheckSpacing(MasterList<CsToken> tokens, bool v, object p)
+		{
+			for (Node<CsToken> tokenNode = tokens.First; tokenNode != null; tokenNode = tokenNode.Next)
+			{
+				if (tokenNode.Value.CsTokenClass == CsTokenClass.Whitespace &&
+					tokenNode.Value.Text.Contains("　"))
+				{
+					this.Violate(tokenNode.Value);
+				}
+			}
+		}
+	}
+}
---
+++ StyleCopCustomUnitTest/Resources/WideSpaceNotBeUsedTestClass.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace StyleCopCustomUnitTest.Resources
+{
+	public static class TrailingSpacesMustNotBeUsedTestClass
+	{
+		public void OK1()
+		{
+			return;
+		}
+
+		public　void NG1()
+		{
+		　　return;
+		}
+	}
+}
