3f5aaa69bda32d0d30722d3797b498a0649c69ac
LAVN <rairairakurakulilac@hotmail.co.jp>
2016-10-18 23:19:48 +0900

    StyleCopCustomプロジェクトでカスタムルール (TrailingSpacesMustNotBeUsed) を作成

---
+++ StyleCopCustom/Rules/TrailingSpacesMustNotBeUsed.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.ObjectModel;
+using StyleCop;
+using StyleCop.CSharp;
+using StyleCopCustom.Settings;
+
+namespace StyleCopCustom.Rules
+{
+	[SourceAnalyzer(typeof(CsParser), "StyleCopCustom.Rules.xml")]
+	public class TrailingSpacesMustNotBeUsed : SourceAnalyzer
+	{
+		private static readonly ReadOnlyCollection<XPathRule> rules;
+
+		static TrailingSpacesMustNotBeUsed()
+		{
+			rules = StyleCopsettingsReader.Settings.XPathRules;
+		}
+
+		public override void AnalyzeDocument(CodeDocument document)
+		{
+			CsDocument csdocument = (CsDocument)document;
+			if (csdocument.RootElement != null && !csdocument.RootElement.Generated)
+			{
+				this.CheckSpacing(csdocument.Tokens, false, null);
+			}
+		}
+
+		private void CheckSpacing(MasterList<CsToken> tokens, bool v, object p)
+		{
+			for (Node<CsToken> tokenNode = tokens.First; tokenNode != null; tokenNode = tokenNode.Next)
+			{
+				if ((tokenNode.Next == null || tokenNode.Next.Value.CsTokenType == CsTokenType.EndOfLine) &&
+					tokenNode.Value.CsTokenClass == CsTokenClass.Whitespace)
+				{
+					this.Violate(tokenNode.Value);
+				}
+			}
+		}
+	}
+}
---
+++ StyleCopCustomUnitTest/Resources/TrailingSpacesMustNotBeUsedTestClass.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace StyleCopCustomUnitTest.Resources
+{
+	public static class TrailingSpacesMustNotBeUsedTestClass
+	{
+		public void OK1()
+		{
+			return;
+		}
+
+		public void NG1()
+		{
+			return;  
+		}
+
+		public void NG2()
+		{
+			return;	
+		}
+
+		public void NG3()
+		{
+			return;　
+		}
+	}
+}
