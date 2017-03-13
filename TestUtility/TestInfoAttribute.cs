2c6957a38f141eddec825886ab36965ae015111f
LAVN <rairairakurakulilac@hotmail.co.jp>
2017-03-13 22:00:42 +0900

    TestInfoAttribute追加

---
+++ TestUtility/TestInfoAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TestUtility
+{
+	[AttributeUsage(AttributeTargets.Method)]
+	public class TestInfoAttribute : Attribute
+	{
+		public TestResult Result { get; set; }
+	}
+}
