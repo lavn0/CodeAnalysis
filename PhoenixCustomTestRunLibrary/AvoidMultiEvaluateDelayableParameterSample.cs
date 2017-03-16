f12220b93ccec3feb21da35a6bed6f7f35d8d17f
LAVN <rairairakurakulilac@hotmail.co.jp>
2017-03-16 20:33:47 +0900

    ユニットテスト修正（AvoidMultiEvaluateDelayableParameter）

--- PhoenixCustomTestRunLibrary/AvoidMultiEvaluateDelayableParameterSample.cs
+++ PhoenixCustomTestRunLibrary/AvoidMultiEvaluateDelayableParameterSample.cs
@@ -1,17 +1,21 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using PhoenixCustom;
+using TestUtility;
 
 namespace PhoenixCustomTestRunLibrary
 {
 	public class AvoidMultiEvaluateDelayableParameterSample
 	{
 		// 1回だけ評価するのはOK
+		[TestInfo(TargetRuleName = nameof(AvoidMultiEvaluateDelayableParameter),  ViolationCount = 0)]
 		public static object OK(IEnumerable<string> items)
 		{
 			return items.ToList();
 		}
 
 		// 2回評価するのはNG
+		[TestInfo(TargetRuleName = nameof(AvoidMultiEvaluateDelayableParameter),  ViolationCount = 1)]
 		public static object NG1(IEnumerable<string> items)
 		{
 			if (items.Any())
@@ -23,6 +27,7 @@ namespace PhoenixCustomTestRunLibrary
 		}
 
 		// 変数に結果をキャッシュすれば２回評価されないのでOK
+		[TestInfo(TargetRuleName = nameof(AvoidMultiEvaluateDelayableParameter),  ViolationCount = 0)]
 		public static object OK1(IEnumerable<string> items)
 		{
 			var item = items.FirstOrDefault();
@@ -35,6 +40,7 @@ namespace PhoenixCustomTestRunLibrary
 		}
 
 		// ２つ評価式があっても同じパスで２回評価されていない場合はOK
+		[TestInfo(TargetRuleName = nameof(AvoidMultiEvaluateDelayableParameter),  ViolationCount = 0)]
 		public static object OK2(IEnumerable<string> items, bool flg)
 		{
 			if (flg)
@@ -48,6 +54,7 @@ namespace PhoenixCustomTestRunLibrary
 		}
 
 		// ループ内の再評価は未判定
+		[TestInfo(TargetRuleName = nameof(AvoidMultiEvaluateDelayableParameter),  ViolationCount = 0)]
 		public static object OK3(IEnumerable<string> items)
 		{
 			var ls = new List<string>();
@@ -60,6 +67,7 @@ namespace PhoenixCustomTestRunLibrary
 		}
 
 		// 関係ない型の引数
+		[TestInfo(TargetRuleName = nameof(AvoidMultiEvaluateDelayableParameter),  ViolationCount = 1)]
 		public static object NG4(List<string> items)
 		{
 			if (items.Any())
--- PhoenixCustomUnitTest/UnitTest.cs
+++ PhoenixCustomUnitTest/UnitTest.cs
@@ -13,10 +13,22 @@ namespace PhoenixCustomUnitTest
 			Assert.AreEqual(0, this.GetErrors("AvoidMultiEvaluateDelayableLocal", "AvoidMultiEvaluateDelayableLocalSample", "OK()").Count, "OK()");
 			Assert.AreEqual(1, this.GetErrors("AvoidMultiEvaluateDelayableLocal", "AvoidMultiEvaluateDelayableLocalSample", "NG1()").Count, "NG1()");
 			Assert.AreEqual(0, this.GetErrors("AvoidMultiEvaluateDelayableLocal", "AvoidMultiEvaluateDelayableLocalSample", "OK1()").Count, "OK1()");
-			Assert.AreEqual(0, this.GetErrors("AvoidMultiEvaluateDelayableLocal", "AvoidMultiEvaluateDelayableLocalSample", "OK2(flg)").Count, "OK2(flg)");
+			Assert.AreEqual(0, this.GetErrors("AvoidMultiEvaluateDelayableLocal", "AvoidMultiEvaluateDelayableLocalSample", "OK2(System.Boolean)").Count, "OK2(System.Boolean)");
 			Assert.AreEqual(0, this.GetErrors("AvoidMultiEvaluateDelayableLocal", "AvoidMultiEvaluateDelayableLocalSample", "NG3()").Count, "NG3()");
 			Assert.AreEqual(0, this.GetErrors("AvoidMultiEvaluateDelayableLocal", "AvoidMultiEvaluateDelayableLocalSample", "OK4()").Count, "OK4()");
 			Assert.AreEqual(1, this.GetErrors("AvoidMultiEvaluateDelayableLocal", "AvoidMultiEvaluateDelayableLocalSample").Count);
+		}
+
+		[TestMethod]
+		public void AvoidMultiEvaluateDelayableParameterSample()
+		{
+			Assert.AreEqual(0, this.GetErrors("AvoidMultiEvaluateDelayableParameter", "AvoidMultiEvaluateDelayableParameterSample", "OK(System.Collections.Generic.IEnumerable`1<System.String>)").Count, "OK(System.Collections.Generic.IEnumerable`1<System.String>)");
+			Assert.AreEqual(1, this.GetErrors("AvoidMultiEvaluateDelayableParameter", "AvoidMultiEvaluateDelayableParameterSample", "NG1(System.Collections.Generic.IEnumerable`1<System.String>)").Count, "NG1(System.Collections.Generic.IEnumerable`1<System.String>)");
+			Assert.AreEqual(0, this.GetErrors("AvoidMultiEvaluateDelayableParameter", "AvoidMultiEvaluateDelayableParameterSample", "OK1(System.Collections.Generic.IEnumerable`1<System.String>)").Count, "OK1(System.Collections.Generic.IEnumerable`1<System.String>)");
+			Assert.AreEqual(0, this.GetErrors("AvoidMultiEvaluateDelayableParameter", "AvoidMultiEvaluateDelayableParameterSample", "OK2(System.Collections.Generic.IEnumerable`1<System.String>,System.Boolean)").Count, "OK2(System.Collections.Generic.IEnumerable`1<System.String>,System.Boolean)");
+			Assert.AreEqual(0, this.GetErrors("AvoidMultiEvaluateDelayableParameter", "AvoidMultiEvaluateDelayableParameterSample", "OK3(System.Collections.Generic.IEnumerable`1<System.String>)").Count, "OK3(System.Collections.Generic.IEnumerable`1<System.String>)");
+			Assert.AreEqual(1, this.GetErrors("AvoidMultiEvaluateDelayableParameter", "AvoidMultiEvaluateDelayableParameterSample", "NG4(System.Collections.Generic.List`1<System.String>)").Count, "NG4(System.Collections.Generic.List`1<System.String>)");
+			Assert.AreEqual(2, this.GetErrors("AvoidMultiEvaluateDelayableParameter", "AvoidMultiEvaluateDelayableParameterSample").Count);
 		}
 	}
 }
