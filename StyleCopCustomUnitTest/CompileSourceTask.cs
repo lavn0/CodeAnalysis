da9d13dd8e2f033168b35d841fa9b226380ecf25
LAVN <rairairakurakulilac@hotmail.co.jp>
2017-03-29 23:33:02 +0900

    CompileSourceタスク作成

---
+++ StyleCopCustomUnitTest/CompileSourceTask.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace TestUtility
+{
+	public class CompileSourceTask : Task
+	{
+		[Required]
+		public string OutputPath { get; set; }
+
+		[Required]
+		public ITaskItem[] InputTestSources { get; set; }
+
+		public override bool Execute()
+		{
+			foreach (var item in InputTestSources)
+			{
+				this.Log.LogMessage("ItemSpec = " + item.ItemSpec);
+				var sourceText = File.ReadAllText(item.ItemSpec);
+				var outputText = Regex.Replace(sourceText, @"^\s*//\s*COMMENT:.*?\n", string.Empty, RegexOptions.Multiline);
+				var outputPath = Path.Combine(OutputPath, item.ItemSpec);
+				Directory.CreateDirectory(Path.GetDirectoryName(outputPath));
+				File.WriteAllText(outputPath, outputText);
+			}
+
+			this.Log.LogCommandLine("CustomTask parsed " + InputTestSources.Length + " files.");
+			return true;
+		}
+	}
+}
