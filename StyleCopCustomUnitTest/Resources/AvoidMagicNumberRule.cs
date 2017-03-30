6d21f2fc98d730c759c4f6e5edf97b15a884e513
LAVN <rairairakurakulilac@hotmail.co.jp>
2017-03-30 22:26:46 +0900

    テストの検証コード追加(StyleCopCustomUnitTest)

--- StyleCopCustomUnitTest/Resources/AvoidMagicNumberRule.cs
+++ StyleCopCustomUnitTest/Resources/AvoidMagicNumberRule.cs
@@ -26,22 +26,22 @@
 		public void NG1()
 		{
 			// 固定値で指定すれば、マジックナンバー扱い
-			// ERROR()
-			// ERROR()
-			// ERROR()
-			// ERROR()
-			// ERROR()
+			// ERROR(48,54) "value"
+			// ERROR(57,58) 10
+			// ERROR(61,64) true
+			// ERROR(67,71) false
+			// ERROR(74,77) null
 			AvoidMagicNumberRuleTestClass.ViolateMethod("value", 10, true, false, null);
 		}
 
 		public void NG2()
 		{
 			// 式も固定値と判断できればNG
-			// ERROR()
-			// ERROR()
-			// ERROR()
-			// ERROR()
-			// ERROR()
+			// ERROR(48,64) "va" + "l" + "ue"
+			// ERROR(67,78) 10 * 10 * 10
+			// ERROR(81,99) true & false | true
+			// ERROR(102,123) false && true || false
+			// ERROR(126,145) true & false || true
 			AvoidMagicNumberRuleTestClass.ViolateMethod("va" + "l" + "ue", 10 * 10 * 10, true & false | true, false && true || false, true & false || true);
 		}
 
--- StyleCopCustomUnitTest/UnitTest.cs
+++ StyleCopCustomUnitTest/UnitTest.cs
@@ -1,6 +1,5 @@
 ﻿using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using StyleCop.CSharp;
 using StyleCopContrib.Runner;
 
 namespace StyleCopCustomUnitTest
@@ -10,23 +9,42 @@ namespace StyleCopCustomUnitTest
 	{
 		private const string settingPath = "NullSettings.StyleCop";
 
-
 		[TestMethod]
 		public void AvoidMagicNumberRule()
 		{
 			var result = StyleCopUtil.RunStyleCop(settingPath, @"Resources\AvoidMagicNumberRule.cs");
 			var violations = result.Violations.Where(v => v.Rule.Name == "AvoidMagicNumberRule").ToList();
 
-			Assert.AreEqual(29, violations[0].Line);
-			Assert.AreEqual(29, violations[1].Line);
-			Assert.AreEqual(29, violations[2].Line);
-			Assert.AreEqual(29, violations[3].Line);
-			Assert.AreEqual(29, violations[4].Line);
-			Assert.AreEqual(35, violations[5].Line);
-			Assert.AreEqual(35, violations[6].Line);
-			Assert.AreEqual(35, violations[7].Line);
-			Assert.AreEqual(35, violations[8].Line);
-			Assert.AreEqual(35, violations[9].Line);
+			Assert.AreEqual(29, violations[0].Line, "エラーの開始行が期待値と異なります。");
+			Assert.AreEqual(48, violations[0].Location.Value.StartPoint.IndexOnLine, "エラーの開始位置が期待値と異なります。");
+			Assert.AreEqual(54, violations[0].Location.Value.EndPoint.IndexOnLine, "エラーの終了位置が期待値と異なります。");
+			Assert.AreEqual(29, violations[1].Line, "エラーの開始行が期待値と異なります。");
+			Assert.AreEqual(57, violations[1].Location.Value.StartPoint.IndexOnLine, "エラーの開始位置が期待値と異なります。");
+			Assert.AreEqual(58, violations[1].Location.Value.EndPoint.IndexOnLine, "エラーの終了位置が期待値と異なります。");
+			Assert.AreEqual(29, violations[2].Line, "エラーの開始行が期待値と異なります。");
+			Assert.AreEqual(61, violations[2].Location.Value.StartPoint.IndexOnLine, "エラーの開始位置が期待値と異なります。");
+			Assert.AreEqual(64, violations[2].Location.Value.EndPoint.IndexOnLine, "エラーの終了位置が期待値と異なります。");
+			Assert.AreEqual(29, violations[3].Line, "エラーの開始行が期待値と異なります。");
+			Assert.AreEqual(67, violations[3].Location.Value.StartPoint.IndexOnLine, "エラーの開始位置が期待値と異なります。");
+			Assert.AreEqual(71, violations[3].Location.Value.EndPoint.IndexOnLine, "エラーの終了位置が期待値と異なります。");
+			Assert.AreEqual(29, violations[4].Line, "エラーの開始行が期待値と異なります。");
+			Assert.AreEqual(74, violations[4].Location.Value.StartPoint.IndexOnLine, "エラーの開始位置が期待値と異なります。");
+			Assert.AreEqual(77, violations[4].Location.Value.EndPoint.IndexOnLine, "エラーの終了位置が期待値と異なります。");
+			Assert.AreEqual(35, violations[5].Line, "エラーの開始行が期待値と異なります。");
+			Assert.AreEqual(48, violations[5].Location.Value.StartPoint.IndexOnLine, "エラーの開始位置が期待値と異なります。");
+			Assert.AreEqual(64, violations[5].Location.Value.EndPoint.IndexOnLine, "エラーの終了位置が期待値と異なります。");
+			Assert.AreEqual(35, violations[6].Line, "エラーの開始行が期待値と異なります。");
+			Assert.AreEqual(67, violations[6].Location.Value.StartPoint.IndexOnLine, "エラーの開始位置が期待値と異なります。");
+			Assert.AreEqual(78, violations[6].Location.Value.EndPoint.IndexOnLine, "エラーの終了位置が期待値と異なります。");
+			Assert.AreEqual(35, violations[7].Line, "エラーの開始行が期待値と異なります。");
+			Assert.AreEqual(81, violations[7].Location.Value.StartPoint.IndexOnLine, "エラーの開始位置が期待値と異なります。");
+			Assert.AreEqual(99, violations[7].Location.Value.EndPoint.IndexOnLine, "エラーの終了位置が期待値と異なります。");
+			Assert.AreEqual(35, violations[8].Line, "エラーの開始行が期待値と異なります。");
+			Assert.AreEqual(102, violations[8].Location.Value.StartPoint.IndexOnLine, "エラーの開始位置が期待値と異なります。");
+			Assert.AreEqual(123, violations[8].Location.Value.EndPoint.IndexOnLine, "エラーの終了位置が期待値と異なります。");
+			Assert.AreEqual(35, violations[9].Line, "エラーの開始行が期待値と異なります。");
+			Assert.AreEqual(126, violations[9].Location.Value.StartPoint.IndexOnLine, "エラーの開始位置が期待値と異なります。");
+			Assert.AreEqual(145, violations[9].Location.Value.EndPoint.IndexOnLine, "エラーの終了位置が期待値と異なります。");
 			Assert.AreEqual(10, violations.Count);
 		}
 
